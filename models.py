8155945c64f4c4091ec5414268f29ae90a42d335
Mehak19206 <pray08819@gmail.com>
2026-04-12 22:17:44 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
+from typing import Literal
 
 class Observation(BaseModel):
-    knowledge: int
-    time_spent: int
+    knowledge: float
+    time_spent: float
+
+class Action(BaseModel):
+    action: Literal["easy", "medium", "hard", "revise", "skip"]
 
 class Reward(BaseModel):
     value: float
